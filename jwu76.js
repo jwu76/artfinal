36a17a9bb8aae681e0dc268fc693d0010dd1be08
Jin Wu <jwu76@uic.edu>
2018-04-27 18:30:34 +0000

    added functionality to search by crime and renamed main page to index.html

--- jwu76.js
+++ jwu76.js
@@ -13,7 +13,15 @@ $(document).ready(function() {
         showMap.style.display = "block";
         screen.style.display = "none";
     });
-
+    
+    var map2 = document.getElementById("MapCrimeResult");
+    $(map2).on('click', function() {
+        var showMap = document.getElementById("resultMap2");
+        var screen = document.getElementById("formInit");
+        showMap.style.display = "block";
+        screen.style.display = "none";
+    });
+    
     var back12 = document.getElementById("back1");
     $(back12).on('click', function() {
         var showTable = document.getElementById("resultTable");
@@ -28,7 +36,13 @@ $(document).ready(function() {
         showMap.style.display = "none";
         screen.style.display = "block";
     });
-
+    var back33 = document.getElementById("back3");
+    $(back33).on('click', function() {
+        var showMap = document.getElementById("resultMap2");
+        var screen = document.getElementById("formInit");
+        showMap.style.display = "none";
+        screen.style.display = "block";
+    });
     var search = document.getElementById("search");
     $(search).on('click', function() {
 
@@ -39,12 +53,21 @@ $(document).ready(function() {
         title.value += year.value;
         initMap();
     });
+   var searchC = document.getElementById("searchCrime");
+    $(searchC).on('click', function() {
 
+        var title = document.getElementById("crime2");
+        title.value = "";
+        var crime = document.getElementById("crime");
+        console.log(crime.value);
+        title.value += crime.value;
+        crimeMap();
+    });
 });
 
 function initMap() {
     var uluru = { lat: 41.881832, lng: -87.623177 };
-    var map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map (document.getElementById("map"), {
         zoom: 13,
         center: uluru
     });
@@ -75,6 +98,44 @@ function initMap() {
                  });
                  marker.addListener('click', function() {
                     info.open(map, marker);
+                });
+        });
+    });
+}
+
+function crimeMap() {
+    var uluru = { lat: 41.881832, lng: -87.623177 };
+    var map2 = new google.maps.Map (document.getElementById("map2"), {
+        zoom: 13,
+        center: uluru
+    });
+    var crime = document.getElementById("crime");
+    var url = "https://data.cityofchicago.org/resource/6zsd-86xi.json?primary_type=" + $("#crime2").val();
+    $.get(url, function(response) {
+        console.log(url);
+        $.each(response, function(i, v) {
+            var contentString = "Location: " + v.block 
+            + "<br>Date & Time: " + v.date + "<br>Crime: " + v.primary_type + "<br> Took Place In: " + v.location_description 
+            + "<br> Arrested: " + v.arrest;
+                if(v.arrest === true){
+                var marker = new google.maps.Marker ({
+                    position: { lat: parseFloat(v.latitude), lng: parseFloat(v.longitude) },
+                    map: map2,
+                    icon: "https://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                });
+                }
+                if(v.arrest === false){
+                    var marker = new google.maps.Marker ({
+                    position: { lat: parseFloat(v.latitude), lng: parseFloat(v.longitude) },
+                    map: map2,
+                    icon: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                });
+                }
+                 var info = new google.maps.InfoWindow({
+                     content: contentString
+                 });
+                 marker.addListener('click', function() {
+                    info.open(map2, marker);
                 });
         });
     });
